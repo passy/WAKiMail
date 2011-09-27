9e82189bb38539269852601b3a7a1bdb7bae93cc
Pascal Hartig (Home) <phartig@rdrei.net>
2011-09-27 11:11:48 +0200

    UI update

--- src/net/rdrei/android/wakimail/ui/WAKiMailActivity.java
+++ src/net/rdrei/android/wakimail/ui/WAKiMailActivity.java
@@ -8,6 +8,6 @@ public class WAKiMailActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_login);
     }
 }
