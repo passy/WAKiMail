929fc5ce5d436cd2500b7c34b32148dc3cfa861c
Pascal Hartig (Home) <phartig@rdrei.net>
2011-10-06 09:31:47 +0200

    Yey, progress dialog works

--- src/net/rdrei/android/wakimail/WAKiMailApplication.java
+++ src/net/rdrei/android/wakimail/WAKiMailApplication.java
@@ -1,7 +1,23 @@
 package net.rdrei.android.wakimail;
 
+import java.util.List;
+
+import net.rdrei.android.wakimail.guice.WAKiMailModule;
+
+import com.google.inject.Module;
+
 import roboguice.application.RoboApplication;
+import roboguice.util.Ln;
 
 public class WAKiMailApplication extends RoboApplication {
 
+	@Override
+	protected void addApplicationModules(List<Module> modules) {
+		super.addApplicationModules(modules);
+		
+		Ln.d("Adding application modules.");
+		
+		modules.add(new WAKiMailModule());
+	}
+	
 }
