5557b4e0cad5410cddaeba27e7aeb1c93979b773
Pascal Hartig <phartig@rdrei.net>
2012-03-24 22:08:17 +0100

    Enable StrictMode in DEBUG env, disable ACRA there

--- WAKiMail/src/main/java/net/rdrei/android/wakimail/WAKiMailApplication.java
+++ WAKiMail/src/main/java/net/rdrei/android/wakimail/WAKiMailApplication.java
@@ -4,20 +4,34 @@ import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 /**
  * Sets up settings that are required for the application to run.
  * 
  * @author pascal
  */
-@ReportsCrashes(
-	formUri = "http://www.bugsense.com/api/acra?api_key=2b0a6e7c",
-	formKey = "")
+@ReportsCrashes(formUri = "http://www.bugsense.com/api/acra?api_key=2b0a6e7c", formKey = "")
 public class WAKiMailApplication extends Application {
 	@Override
 	public void onCreate() {
-		// The following line triggers the initialization of ACRA
-		ACRA.init(this);
 		super.onCreate();
+
+		if (BuildConfig.DEBUG) {
+			enableStrictMode();
+		} else {
+			// Only if DEBUG is disabled.
+			ACRA.init(this);
+		}
+	}
+
+	private void enableStrictMode() {
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+				.detectAll().penaltyLog().build());
+		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+				.detectLeakedSqlLiteObjects()
+				// Only on 11+ if I'm not mistaken.
+				.detectLeakedClosableObjects().penaltyLog().penaltyDeath()
+				.build());
 	}
 }
